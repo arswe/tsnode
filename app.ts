559132015656a41075fd54fcd7e5cc9b64261f68
arswe <202015014@student.green.ac.bd>
2024-02-26 19:26:21 +0600

    remindersRouter

--- app.ts
+++ app.ts
@@ -1,9 +1,13 @@
 import colors from 'colors'
+import dotenv from 'dotenv'
 import express from 'express'
 import remindersRouter from './src/routes/remindersRouter'
 
 const app = express()
-const port = 8080
+dotenv.config()
+app.use(express.json())
+
+const port = process.env.PORT || 8080
 
 app.use('/api/reminders', remindersRouter)
 
