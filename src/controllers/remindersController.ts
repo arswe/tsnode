365500da1ce2da8eff8445de326e016af1adb2ed
arswe <202015014@student.green.ac.bd>
2024-02-26 21:31:47 +0600

    getReminders

--- src/controllers/remindersController.ts
+++ src/controllers/remindersController.ts
@@ -1,14 +1,15 @@
+import { Request, Response } from 'express'
+import asyncHandler from '../middlewares/asyncHandler'
+
 // @docs  Fetch All reminder
 // @Route GET /api/reminders
 // @access  Public
 
-import asyncHandler from '../middlewares/asyncHandler'
-
-const getReminders = asyncHandler(async (req, res) => {
+const getReminders = asyncHandler(async (req: Request, res: Response) => {
   res.send('Hello reminders!')
 })
 
-const getReminderById = asyncHandler(async (req, res) => {
+const getReminderById = asyncHandler(async (req: Request, res: Response) => {
   res.send('Hello reminders!')
 })
 
