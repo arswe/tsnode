25cd571e15925ecc259f365710d9657122c953af
arswe <202015014@student.green.ac.bd>
2024-02-26 21:46:55 +0600

    createReminderReview

--- src/routes/remindersRouter.ts
+++ src/routes/remindersRouter.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express'
-import { createReminder, getReminderById, getReminders } from '../controllers/remindersController'
+import { createReminder, createReminderReview, getReminderById, getReminders } from '../controllers/remindersController'
 
 const router = Router()
 
 router.route('/').get(getReminders).post(createReminder)
+router.route('/:id/reviews').post(createReminderReview)
 router.route('/:id').get(getReminderById)
 
 export default router
