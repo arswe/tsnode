e30839f7cecfadca7252c00d8266a80dac6a7cd6
arswe <202015014@student.green.ac.bd>
2024-02-26 19:30:54 +0600

    reminders

---
+++ data/reminders.ts
@@ -0,0 +1,24 @@
+export default {
+  reminders: [
+    {
+      id: 1,
+      title: 'Meeting with John',
+      date: '2024-03-01T10:00:00',
+      location: '123 Main St',
+      description: 'Discuss project proposal',
+    },
+    {
+      id: 2,
+      title: 'Buy groceries',
+      date: '2024-03-02T18:00:00',
+      location: 'Local supermarket',
+      description: 'Milk, eggs, bread',
+    },
+    {
+      id: 3,
+      title: 'Call Mom',
+      date: '2024-03-03T15:30:00',
+      description: 'Wish her happy birthday',
+    },
+  ],
+}
