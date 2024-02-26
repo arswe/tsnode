6a68320d2bea8b91b7e183504ba59c6d5c169216
arswe <202015014@student.green.ac.bd>
2024-02-26 18:57:15 +0600

    all package ready for practic rest api

--- app.ts
+++ app.ts
@@ -1,1 +1,25 @@
-console.log('akiad')
+import colors from 'colors'
+import express from 'express'
+
+const app = express()
+const port = 8080
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/', (req, res) => {
+  res.send('Got a POST request')
+})
+
+app.put('/user', (req, res) => {
+  res.send('Got a PUT request at /user')
+})
+
+app.delete('/user', (req, res) => {
+  res.send('Got a DELETE request at /user')
+})
+
+app.listen(port, () => {
+  console.log(colors.rainbow(`Backend Running  http://localhost:${port}`).bold)
+})
